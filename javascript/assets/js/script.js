0093c0c412aefc7fc0cda92a3807e235ead50004
Fatime668 <tu7dkjdue@code.edu.az>
2022-05-04 11:50:41 +0400

    Tasks

---
+++ javascript/assets/js/script.js
@@ -0,0 +1,95 @@
+// Task 1 
+// function Reverse(word) {
+//     for (let i = word.length-1; i >= 0; i--) {
+//            console.log(word[i]);
+//     }
+// }
+// Reverse("Fatima Hasanzade");
+
+//************************************************************
+
+//Task2
+// class CustomMatch{
+//     constructor(number,plus,minus,multiply,divide){
+//         this.Number = number;
+//     }
+//     plus = function(a){
+//         return this.Number+a;
+//     }
+//     minus = function(a){
+//         return this.Number-a;
+//     }
+//     multiply = function(a){
+//         return this.Number*a;
+//     }
+//     divide = function(a){
+//         return this.Number/a;
+//     }
+// }
+// var result = new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2)
+// console.log(result);
+
+//************************************************************
+
+//Task4
+// var password = prompt("Enter the password : ")
+// if (password == "password1") {
+//     console.log("correct");
+// }else{
+//     console.log("incorrect");
+// }
+// var username = prompt("Enter the Username : ")
+// if (username == "username1") {
+//     console.log("correct");
+// }else{
+//     console.log("incorrect");
+// }
+
+//************************************************************
+
+//Task7
+// arr = [1,30,20,22,45,80,100]
+// function FindIndex(params) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == 45) {
+//             return i
+//         }        
+//     }
+// }
+// console.log(FindIndex());
+
+//************************************************************
+
+//Task6
+// let power =(number,n)=>{
+//     if (n === 0) {
+//         return 1;
+//     }
+//     let result = 1;
+//     while (n != 0) {
+//         n--;
+//         result *=number
+//     }
+//     return result
+// }
+// console.log(power(2,1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
